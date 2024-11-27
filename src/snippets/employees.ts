64a8031b93ba371b17044a59c31816ecc1249918
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-27 14:03:46 +0100

    feat: partial helper

--- src/snippets/employees.ts
+++ src/snippets/employees.ts
@@ -30,7 +30,28 @@ const superManager: SuperBossOrSuperManager = {
 type User = {
   name: string;
   // age: number | undefined; // function, business logic, state, not component
-  age?: number;
+  age: number;
+  email: string;
 };
 
+// type PartialUser = Partial<User>;
+// type UserKeys = keyof User;
+
+type MyPartial<T> = {
+  [K in keyof T]?: T[K];
+};
+type PartialUser = MyPartial<User>;
+
+type UserKeys = keyof User;
+// declare const userKeys: UserKeys = 'name';
+
 // declare createUser(user: User); // { name: 'abc', age: undefined}, {name: 'abc', age: 123}
+
+const config = {
+  apiUrl: 'https://api.example.com',
+  timeout: 5000,
+}; // as const;
+
+type Config = typeof config;
+
+config.apiUrl = 'https://evil.com';
