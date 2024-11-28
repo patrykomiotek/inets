55df724a9cf68e1359e9a203ce5a9d81b4f1780d
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-28 14:21:50 +0100

    feat: with decorators

--- src/examples/06-decorators/01-example-decorator.ts
+++ src/examples/06-decorators/01-example-decorator.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+
 function controller(constructor: Function) {
   console.log(`${constructor.name} was used`);
 }
@@ -16,15 +18,60 @@ function log(
   };
 }
 
+function logParameter(
+  target: any,
+  propertyKey: string,
+  parameterIndex: number,
+) {
+  console.log({ target, propertyKey, parameterIndex });
+
+  if (typeof target !== 'object') {
+    console.warn('Invalid target for property decorator');
+  }
+
+  const existingParameters =
+    Reflect.getOwnMetadata('parameters', target, propertyKey) || [];
+
+  console.log({ existingParameters });
+
+  existingParameters.push(parameterIndex);
+
+  Reflect.defineMetadata('parameters', existingParameters, target, propertyKey);
+}
+
+function positive(target: any, propertyKey: string) {
+  let value: number;
+
+  const getter = () => value;
+
+  const setter = (newValue: number) => {
+    if (newValue < 0) {
+      throw new Error(`Invalid value for ${propertyKey}: must be positive.`);
+    }
+    value = newValue;
+  };
+
+  Object.defineProperty(target, propertyKey, {
+    get: getter,
+    set: setter,
+  });
+}
+
 @controller
 class Example {
+  // @Length(10)
   car: string = 'audi';
 
+  @positive
+  balance: number = 0;
+
+  // @isPositive -> throw new Error
   @log
-  someMethod(arg: number) {
+  someMethod(@logParameter arg: number, @logParameter arg2: string) {
     return arg * 2;
   }
 }
 
 const example = new Example();
-example.someMethod(1);
+example.someMethod(1, 'lorem ipsum');
+example.balance = -200;
