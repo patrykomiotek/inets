0af67b9b11246dbada236a8426720038a5f97738
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-11-29 16:16:05 +0100

    feat: with branded types

---
+++ src/examples/11-branded-types/05-currencies.ts
@@ -0,0 +1,17 @@
+/**
+ * Stwórz system branded types dla różnych walut (np. USD, EUR, GBP).
+ * Napisz funkcję konwersji walut, która akceptuje źródłową walutę,
+ * kwotę i docelową walutę.
+ */
+
+// your code here
+
+// ues cases:
+const usd = createCurrency(100, 'USD');
+const gbp = createCurrency(50, 'GBP');
+// const pln = createCurrency(100, 'PLN');
+
+const eur = convertCurrency(usd, 'EUR', 0.85); // Konwersja USD na EUR
+
+addCurrencies(usd, eur); // Powinno zgłosić błąd kompilacji (różne waluty)
+addCurrencies(usd, createCurrency(200, 'USD')); // Powinno działać
